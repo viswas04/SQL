7153938c3165a1fed8d874e3e138a77d3c00d3fd
venkataviswasmolugu@gmail.com <venkataviswasmolugu@gmail.com>
2025-08-21 17:38:21 +0530

    ...

---
+++ commands.sql
@@ -0,0 +1,21 @@
+//COMMANDS TO CREATE A DATABASE
+CREATE DATABASE database_name;
+Ex: create database mydb;
+
+//COMMANDS TO CREATE A TABLE
+CREATE TABLE table_name (
+    column1 datatype,
+    column2 datatype,
+    ...
+);
+
+//COMMANDS TO INSERT DATA INTO A TABLE
+INSERT INTO table_name (column1, column2, ...)
+VALUES (value1, value2, ...);
+
+//COMMANDS TO UPDATE DATA IN A TABLE
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+
